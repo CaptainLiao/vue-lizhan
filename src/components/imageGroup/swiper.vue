6bb896f0ceffb1413992f55fbc257621b76e3c81
15196004316@163.com <lfy@0916>
2017-07-05 14:22:20 +0800

    complete image swiper widget

--- src/components/imageGroup/swiper.vue
+++ src/components/imageGroup/swiper.vue
@@ -8,16 +8,16 @@
          :style="{transform}"
          ref="swipeArea"
     >
-      <!--<div class="fui-banner-item">-->
-        <!--<img src="http://oms.reedhuabo.com/uploads/ad/20170630/004580.jpg" alt="">-->
-      <!--</div>-->
+      <div class="fui-banner-item">
+        <img src="http://oms.reedhuabo.com/uploads/ad/20170630/004580.jpg" alt="">
+      </div>
       <div class="fui-banner-item" v-for="item in list">
         <img :src="host + item.url" alt="">
       </div>
 
-      <!--<div class="fui-banner-item">-->
-        <!--<img src="http://oms.reedhuabo.com/uploads/ad/20170704/004586.jpg" alt="">-->
-      <!--</div>-->
+      <div class="fui-banner-item">
+        <img src="http://oms.reedhuabo.com/uploads/ad/20170704/004586.jpg" alt="">
+      </div>
       <!--<div class="fui-banner-item">-->
         <!--<img src="../../assets/logo.png" alt="">-->
       <!--</div>-->
@@ -28,10 +28,10 @@
     </div>
     <ul class="fui-circle" v-if="showCircle">
       <li
-        v-for="item,index in list"
+        v-for="item,curIndex in list"
         @click="switchImages(index)"
       >
-        <span class="fui-circle-item" :class="(index === next) ? 'li-on' : ''"></span>
+        <span class="fui-circle-item" :class="(curIndex + 1 === index) ? 'li-on' : ''"></span>
       </li>
     </ul>
   </div>
@@ -44,12 +44,15 @@
         host: this.$host,
 
         // 图片轮播参数
+        // 客户端宽度
         clientWidth: 0,
+        // 图片张数
         imgLen: '',
         startX: 0,
         offsetLeft: 0,
         transform: '',
-        next:0,
+        // 控制 circle button
+        index:1,
       }
     },
     props: {
@@ -59,6 +62,10 @@
         default: true
       },
       autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      infiniteLoop: {
         type: Boolean,
         default: true
       }
@@ -74,20 +81,22 @@
     mounted() {
       let clientWidth = document.documentElement.clientWidth;
 
+      this.touchMoving(-clientWidth);
       this.clientWidth = clientWidth;
+      this.offsetLeft = -clientWidth;
     },
 
     methods: {
       touchStart(e) {
-        let startX = e.changedTouches[0].clientX;
-
+        let startX = e.changedTouches[0].pageX;
+        if(this.imgLen.length <= 0) return;
         this.startX = startX;
         this.offsetLeft = startX - this.offsetLeft;
       },
       touchMove(e) {
         let _this = this;
         let offsetLeft = _this.offsetLeft;
-        let moveX = e.changedTouches[0].clientX;
+        let moveX = e.changedTouches[0].pageX;
         let offsetX = moveX - offsetLeft;
 
         this.touchMoving(offsetX);
@@ -95,45 +104,57 @@
       touchEnd(e) {
         let _this = this;
         let startX = _this.startX;
-        let endX = e.changedTouches[0].clientX;
+        let endX = e.changedTouches[0].pageX;
         let offsetX = endX - startX;
         _this.touchEnded(offsetX);
       },
       touchMoving(offsetX) {
-        this.transform = 'translateX('+offsetX+'px)';
+        this.transform = 'translate3D('+offsetX+'px, 0, 0)';
       },
       touchEnded(offsetX) {
         let clientWidth = this.clientWidth;
-        let index = this.next;
+        let index = this.index;
         let imgLen = this.imgLen;
 
-        if(Math.abs(offsetX) > clientWidth / 3) {
+        if(Math.abs(offsetX) > clientWidth / 4) { // 开始滑动
           if(offsetX > 0) { // 右滑动
 
              --index;
-            if(index <= 0) index = 0;
             offsetX = -clientWidth * index;
-            this.next = index;
-
           }else { // 左滑动
+
             ++index;
-            if(index >= imgLen) index = imgLen - 1;
             offsetX = -clientWidth * index;
-            this.next = index;
           }
-        } else {
+        } else {  // 不滑动
           offsetX = -clientWidth * index;
         }
 
-        this.transform = 'translateX('+offsetX+'px)';
-        this.offsetLeft = offsetX;
+        this.touchMoving(offsetX);
+
+        // 滑动边界，开启循环模式
+        // 关键在于重置图片的 left 值
+        // 如果使用 transition 就能看到切换过程
+        if(index <= 0) {
+          index = imgLen;
+          offsetX = -clientWidth * index;
+          this.touchMoving(offsetX);
+        }
+        if(index >= imgLen + 1) {
+          index = 1;
+          offsetX = -clientWidth * index;
+          this.touchMoving(offsetX);
+        }
+
+        this.index = index;
+        this.offsetLeft = -clientWidth * index
       },
       switchImages(index) {
         let clientWidth = this.clientWidth;
         let offsetX = -clientWidth * index;
 
-        this.transform = 'translateX('+offsetX+'px)';
-        this.next = index;
+        this.transform = 'translate3D('+offsetX+'px, 0, 0)';
+        this.index = index;
         this.offsetLeft = offsetX;
       }
     }
@@ -156,7 +177,8 @@
     z-index: 1;
     display: flex;
     align-item: center;
-    transition: all .3s;
+
+    /*transition: all .3s;*/
 
   }
   .fui-banner-item {
